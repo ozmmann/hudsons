1c832f49dc82ecd213fb028c60bb4da05f0bb8a4
Osman Ramazanov <osman.ramazanov@gmail.com>
2016-11-28 15:39:08 +0200

    responsive one slide

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         autoPlay: 3000,
-        loop:true,
-        margin:10,
+        loop: true,
+        margin: 10,
         items: 1,
         nav: true,
-        navigation : true,
+        navigation: true,
         navigationText: [
             "<i class='icon-chevron-left icon-white'></i>",
             "<i class='icon-chevron-right icon-white'></i>"
         ],
+        singleItem: true,
+        responsive: {
+            0: {
+                items: 1 // Two items from 480px up to 677px
+            }
+        }
     });
 });
